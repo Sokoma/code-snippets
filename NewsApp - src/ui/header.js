80ad019efe74022f443495509e3e854ee63f45de
MartinFalco <34011391+Sokoma@users.noreply.github.com>
2020-10-21 16:41:01 +0200

    Create header.js

---
+++ NewsApp - src/ui/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <p>Lates news from NY times. Use the select box below to filter out articles about specific topic.</p>
+    )
+}
+
+export default Header
